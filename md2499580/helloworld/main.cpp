5aadc4a4a64eb06e2cb1ecf45bc0303eaf5896cd
MDaw <MarwahDaw@gmail.com>
2014-01-08 11:04:13 -0800

    Signed-off-by: MDaw <MarwahDaw@gmail.com>

---
+++ md2499580/helloworld/main.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.cpp
+ * Author: Marwah Dawoody
+ *
+ * Created on January 6, 2014, 9:00 AM
+ */
+//System Libraries 
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Functional Prototypes
+//edit
+//Function Main is where execution
+//begins
+int main(int argc, char** argv) {
+    //Output the string hello world
+    cout<<"helloworld!"<<endl;
+    //Exit Stage right
+    return 0;
+}
+
