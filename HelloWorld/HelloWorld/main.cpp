55372695c0880bf2a152cf5e0516358fa37e1a73
cesaredgar <cesar_velazquez1994@yahoo.com>
2014-01-08 10:54:03 -0800

    Signed-off-by: cesaredgar <cesar_velazquez1994@yahoo.com>

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,24 @@
+/* 
+ * File:   main.cpp
+ * Author: Cesar E. Velazquez
+ *
+ * Created on January 6, 2014, 9:00 AM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Functional Prototypes
+
+//Function Main is where execution begins
+
+int main(int argc, char** argv) {
+    //Output the string Hello World 
+    cout<<"Hello World"<<endl;
+    //Exit Stage right
+    return 0;
+}
+
