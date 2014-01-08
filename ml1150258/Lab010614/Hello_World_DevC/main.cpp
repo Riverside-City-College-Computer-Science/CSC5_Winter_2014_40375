194499ee21e4133e44b2dafc54cac5fa9cc9b3d9
mlehrjava <mark.lehr@rcc.edu>
2014-01-08 07:37:47 -0800

    Signed-off-by: mlehrjava <mark.lehr@rcc.edu>

---
+++ ml1150258/Lab010614/Hello_World_DevC/main.cpp
@@ -0,0 +1,22 @@
+/*
+  Dr. Mark E. Lehr
+  January 6th, 2014
+  First Program in DevC
+*/
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(){
+    //Print Hello World
+    cout<<"Hello World"<<endl;
+    //Exit Stage Right
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
---
+++ ml1150258/Lab010614/Hello_World_NB/main.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on January 6, 2014, 6:13 PM
+ * Our first program in netbeans!
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Print the string Hello World
+    cout<<"Hello World"<<endl;
+    //Exit stage right
+    return 1;
+}
+
