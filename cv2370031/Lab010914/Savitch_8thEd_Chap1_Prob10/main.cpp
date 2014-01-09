2a693f611cfb2eada6ef97d1e21b01e4847bf8b5
cfvasquez19 <cfvasquez19@live.com>
2014-01-09 11:54:20 -0800

    Signed-off-by: cfvasquez19 <cfvasquez19@live.com>

---
+++ cv2370031/Lab010914/Savitch_8thEd_Chap1_Prob10/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on January 9, 2014, 10:29 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    //declare the char variable
+    char letter;
+    //input the letter
+    cout<<"what letter whould you like to use"
+            <<" to out put a capital C."<<endl;
+    cin>>letter;
+    //output the capital C
+    cout       <<" "<<letter<<"  "<<letter<<"  "<<letter<<endl;
+    cout << " "<<letter                    <<" "<<letter<<endl;
+    cout <<letter<<endl;
+    cout <<letter<<endl;
+    cout <<letter<<endl;
+    cout <<letter<<endl;
+    cout <<letter<<endl;
+    cout <<" "<<letter<<         " "<<letter<<endl;
+    cout <<"  "<< letter <<" "<<letter<<endl;
+    
+    //Exit
+    return 0;
+}
+
