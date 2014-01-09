e3dbe898afbf7038fb2b781f8ad01b0129144c4d
ming9275 <edwardyau927@hotmail.com>
2014-01-09 12:12:55 -0800

    Signed-off-by: ming9275 <edwardyau927@hotmail.com>

---
+++ ky2430132/Lab010914_Files_Interpolation/Svitch_8thEd_Chap1_Prob10_NB_010914/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: Kai Ming Yau
+ * Created on January 9, 2014, 10:25 AM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+
+int main(int argc, char** argv) {
+
+    //Declare the char variable
+    
+    char letter;
+    //Output the letter
+    cout<<"What letter would you like to use to output a capital C? "<<endl;
+    cin>>letter;
+    //output the canpital C
+    cout<<"  "<<letter<<" "<<letter<<" "<<letter<<endl;
+    cout<<" "<<letter<<"    "<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<" "<<letter<<"    "<<letter<<endl;
+    cout<<"  "<<letter<<" "<<letter<<" "<<letter<<endl;
+    
+    //Exit stage right
+    return 0;
+}
+
