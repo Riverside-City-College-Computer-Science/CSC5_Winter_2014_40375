16ecfce8ab954a3b44fbaab0d65c9b9233d5e46e
GuohaoLiu <gliu4@student.rcc.edu>
2014-01-09 12:09:10 -0800

    Signed-off-by: GuohaoLiu <gliu4@student.rcc.edu>

---
+++ gl2430121/Lab010914_Files_Interpolation/Savitch_8thEd_Chap1_Prob10/Savitch_8thEd_Chap1_Prob10/main.cpp
@@ -0,0 +1,37 @@
+/* 
+ * File:   main.cpp
+ * Author: Guohao Liu
+ *
+ * Created on January 9, 2014, 10:25 AM
+ */
+//System Libraries
+#include <iostream>
+using namespace std;
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+
+
+int main(int argc, char** argv) {
+//Declare the char variable
+    char letter;
+    //input the letter
+    cout<<"What letter would you like to use "
+            <<"to output a capitial C"<<endl;
+    cin>>letter;
+    //Output the Capital C
+    cout<<"  "<<letter<<" "<<letter<<" "<<letter<<endl;
+    cout<<" "<<letter<<"     "<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<" "<<letter<<"     "<<letter<<endl;
+    cout<<"  "<<letter<<" "<<letter<<" "<<letter<<endl;
+
+    return 0;
+}
+
