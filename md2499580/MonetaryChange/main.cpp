1a87aa36387d4b7b554891c3031f464d9e3f59b2
MDaw <MarwahDaw@gmail.com>
2014-01-15 11:17:47 -0800

    Signed-off-by: MDaw <MarwahDaw@gmail.com>

--- md2499580/MonetaryChange/main.cpp
+++ md2499580/MonetaryChange/main.cpp
@@ -23,24 +23,26 @@ const int F_VAL=1;//1 cent
 int main(int argc, char** argv) {
     //Declare Variables
     float price, amtTend;
-    //loop to ask numurus times for change calculation 
+    //loop to ask numerous times for change calculation 
     do{
         //what is the price of the item
         cout<<"What is the price of the item ($'s)?"<<endl;
         cin>>price;
         //how much was received
-        do{
+    do{
         
-            cout<<"What was received/tendered ($'s)?"<<endl;
-            cout<<"Has to be greater than the price"<<endl;
-            cin>>amtTend;
-        }while(amtTend<price);   
+        cout<<"What was received/tendered ($'s)?"<<endl;
+        cout<<"Has to be greater than the price"<<endl;
+        cin>>amtTend;
+        }while(amtTend<price); 
+        
      //convert to pennies
         int nPrice=price*DLR_VAL;
-        
+        int mtTend=amtTend*DLR_VAL;
+        int change=mtTend-nPrice;
     //calculate how many half dollars
         int nHalf=price/H_VAL;
-        cout<<"Half Dollars ->"<<nHalf<<endl;
+        cout<<"Half Dollars -> "<<nHalf<<endl;
         price-=nHalf*H_VAL;
         cout<<"Would you like to purchase another item (y or n)"<<endl;
             cin>>answer<<endl;
