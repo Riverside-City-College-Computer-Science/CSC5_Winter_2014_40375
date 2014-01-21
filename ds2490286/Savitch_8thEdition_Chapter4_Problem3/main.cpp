9ec1c3d7c12b11cd7b26bcc0bdc0059ab9108678
IEDave <dwsmit@dslextreme.com>
2014-01-21 12:05:19 -0800

    Signed-off-by: IEDave <dwsmit@dslextreme.com>

---
+++ ds2490286/Savitch_8thEdition_Chapter4_Problem3/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: David W. Smith
+ * Created on January 21, 2014, 11:53 AM
+ */
+//System Libraries
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+float stkPric(int,int,int);
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
