09ad6109bce0736c44e46be1e1eaccfc8d2f5ece
mlehrjava <mark.lehr@rcc.edu>
2014-01-28 10:53:58 -0800

    Signed-off-by: mlehrjava <mark.lehr@rcc.edu>

--- ml1150258/Gaddis_7thEd_Chap7_Prob4/main.cpp
+++ ml1150258/Gaddis_7thEd_Chap7_Prob4/main.cpp
@@ -24,20 +24,56 @@ void avg(int [][COLS],int [],int);
 
 int main(int argc, char** argv) {
     //Declare variables
-    const int ROWS=5;
+    const int ROWS=20;
     int mnkFood[ROWS][COLS];
     char fName[]="./monkey.dat";
     //Read the data from the file
     int actSize=read(fName,mnkFood);
     //Write the data
     write(mnkFood,actSize);
+    //Output the minimum amount of food eaten by
+    //a monkey
+    cout<<"Smallest amount eaten by a monkey = "
+        <<min(mnkFood,actSize)<<" ounces"<<endl;
+    //Output the minimum amount of food eaten by
+    //a monkey
+    cout<<"Largest amount eaten by a monkey = "
+        <<max(mnkFood,actSize)<<" ounces"<<endl;
     //Exit stage right
     return 0;
 }
 
+int max(int monkey[][COLS],int ROWS){
+    //Declare some small variable;
+    int big=monkey[0][0];
+    for(int row=0;row<ROWS;row++){
+        for(int col=0;col<COLS;col++){
+            if(big<monkey[row][col]){
+                big=monkey[row][col];
+            }
+        }
+    }
+    return big;
+}
+
+int min(int monkey[][COLS],int ROWS){
+    //Declare some small variable;
+    int small=monkey[0][0];
+    for(int row=0;row<ROWS;row++){
+        for(int col=0;col<COLS;col++){
+            if(small>monkey[row][col]){
+                small=monkey[row][col];
+            }
+        }
+    }
+    return small;
+}
+
 void write(int monkey[][COLS],int ROWS){
     cout<<endl;
+    cout<<"Monkey    M   T   W   T   F   S   S"<<endl;
     for(int row=0;row<ROWS;row++){
+        cout<<"  "<<row+1<<"    ";
         for(int col=0;col<COLS;col++){
             cout<<setw(4)<<monkey[row][col];
         }
