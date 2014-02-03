b6f8092905f4e772423a863b1f48e8236baa3543
mlehrjava <mark.lehr@rcc.edu>
2014-02-03 09:25:27 -0800

    Signed-off-by: mlehrjava <mark.lehr@rcc.edu>

---
+++ ml1150258/Mark_Sort/main.cpp
@@ -0,0 +1,102 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on February 3, 2014, 8:13 AM
+ */
+
+//System Libraries
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+void filAray(int [],int);
+void prntAry(int [],int,int);
+void swap(int &,int &);
+void swap(int [],int,int);
+void minPos(int [],int,int);
+void mrkSort(int [],int);
+void copy(int [],int [],int);
+
+//Executions Begin Here!
+int main(int argc, char** argv) {
+    //Declare variables and initialize the
+    //random number generator
+    const int SIZE=200;
+    int array[SIZE],brray[SIZE];
+    srand(static_cast<unsigned int>(time(0)));
+    //Fill the arrays
+    filAray(array,SIZE);
+    copy(array,brray,SIZE);
+    //Print the array
+    prntAry(array,SIZE,10);
+    //Test out the min pos routine
+    mrkSort(brray,SIZE);
+    //Print the array
+    prntAry(brray,SIZE,10);
+    //Exit Stage Right!!!
+    return 0;
+}
+
+void copy(int a[],int b[],int n){
+    for(int i=0;i<n;i++){
+        b[i]=a[i];
+    }
+}
+
+void mrkSort(int a[],int n){
+    for(int i=0;i<n-1;i++){
+        minPos(a,n,i);
+    }
+}
+
+void minPos(int a[],int n,int pos){
+    for(int i=pos+1;i<n;i++){
+        if(a[pos]>a[i])swap(a[pos],a[i]);
+        //if(a[pos]>a[i])swap(a,pos,i);
+    }
+}
+
+void swap(int &a,int &b){
+    int temp=a;
+    a=b;
+    b=temp;
+}
+
+void swap(int a[],int i,int j){
+    int temp=a[i];
+    a[i]=a[j];
+    a[j]=temp;
+}
+
+void prntAry(int a[],int n,int perLine){
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+        if(i%10==(perLine-1))cout<<endl;
+    }
+    cout<<endl;
+}
+
+//2 Digit random numbers
+void filAray(int a[],int n){
+    for(int i=0;i<n;i++){
+        a[i]=rand()%90+10;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
