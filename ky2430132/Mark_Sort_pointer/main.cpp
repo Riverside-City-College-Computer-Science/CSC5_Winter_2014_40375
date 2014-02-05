2fb29268b5dbd0b99650c924e65be2be9c1e0ac4
ming9275 <edwardyau927@Hotmail.com>
2014-02-05 11:57:31 -0800

    Signed-off-by: ming9275 <edwardyau927@Hotmail.com>

---
+++ ky2430132/Mark_Sort_pointer/main.cpp
@@ -0,0 +1,105 @@
+/* 
+ * File:   main.cpp
+ * Author: Kai Ming Yau
+ * Created on February 5, 2014, 10:10 AM
+ */
+
+//System Libraries
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+void filAray(int *,int);
+void prntAry(const int *,int,int);
+void swap(int &,int &);
+void swap(int *,int,int);
+void minPos(int *,int,int);
+void mrkSort(int *,int);
+void xcopy(const int *,int *,int);
+
+//Executions Begin Here!
+int main(int argc, char** argv) {
+    //Declare variables and initialize the
+    //random number generator
+    const int SIZE=200;
+    int *array=new int [SIZE];
+    int *brray=new int [SIZE];
+    srand(static_cast<unsigned int>(time(0)));
+    //Fill the arrays
+    filAray(array,SIZE);
+    xcopy(array,brray,SIZE);
+    //Print the array
+    prntAry(array,SIZE,10);
+    //Test out the min pos routine
+    mrkSort(brray,SIZE);
+    //Print the array
+    prntAry(brray,SIZE,10);
+    //Exit and clean up
+    delete []array;
+    delete []brray;
+    return 0;
+}
+
+void xcopy(const int *a,int *b,int n){
+    for(int i=0;i<n;i++){
+        *(b+i)=*(a+i);
+    }
+}
+
+void mrkSort(int *a,int n){
+    for(int i=0;i<n-1;i++){
+        minPos(a,n,i);
+    }
+}
+
+void minPos(int *a,int n,int pos){
+    for(int i=pos+1;i<n;i++){
+        if(*(a+pos)>*(a+i))swap(*(a+pos),*(a+i));
+        //if(a[pos]>a[i])swap(a,pos,i);
+    }
+}
+
+void swap(int &a,int &b){
+    int temp=a;
+    a=b;
+    b=temp;
+}
+
+void swap(int *a,int i,int j){
+    int temp=*(a+i);
+    *(a+i)=*(a+j);
+    *(a+j)=temp;
+}
+
+void prntAry(const int *a,int n,int perLine){
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<*(a+i)<<" ";
+        if(i%10==(perLine-1))cout<<endl;
+    }
+    cout<<endl;
+}
+
+//2 Digit random numbers
+void filAray(int *a,int n){
+    for(int i=0;i<n;i++){
+        *(a+i)=rand()%90+10;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
